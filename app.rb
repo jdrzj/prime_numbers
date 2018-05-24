ff6fcd4e12912db08718e69f47a5a9ecf807977f
Jędrzej <jedrek.urbanski@gmail.com>
2018-05-24 12:51:55 +0200

    Remove pry

--- app.rb
+++ app.rb
@@ -3,8 +3,6 @@ $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'lib/generate_primes'
 require 'lib/multiplication_table'
 
-require 'pry'
-
 size = (ARGV[0] || 10).to_i
 
 numbers = GeneratePrimes.get_primes(size)
